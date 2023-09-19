19af85160c2bbee12dbd833f6b78b65886e7b7ed
Mai Mohamed <maim51084@gmail.com>
2023-09-19 23:50:21 +0300

    Update Calculator.cpp

--- Task-2 Simple Calculator/Calculator.cpp
+++ Task-2 Simple Calculator/Calculator.cpp
@@ -7,47 +7,50 @@ using namespace std;
 void solution() {
     char operation;
     double num1, num2, result;
+    char continueChoice;
+    cout << "Welcome to Simple Calculator Program :)" << endl;
+    do {
+        cout << "Enter the first number: ";
+        cin >> num1;
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Choose an operation (+, -, *, /): ";
+        cin >> operation;
 
-    cout << "Welcome to Simple Calculator Program!" << endL;
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
-    cout << "Choose an operation (+, -, *, /): ";
-    cin >> operation;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                cout << num1 << " + " << num2 << " = " << result << endl;
+                break;
+            case '-':
+                result = num1 - num2;
+                cout << num1 << " - " << num2 << " = " << result << endl;
+                break;
+            case '*':
+                result = num1 * num2;
+                cout << num1 << " * " << num2 << " = " << result << endl;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    cout << num1 << " / " << num2 << " = " << result << endl;
+                }
+                else
+                    cout << "INVALID! Division by zero." << endl;
+                break;
+            default:
+                cout << "Invalid operation! Please choose from (+, -, *, /)." << endl;
+                break;
+        }
 
-    switch (operation) {
-        case '+':
-            result = num1 + num2;
-            cout << num1 << " + " << num2 << " = " << result << endL;
-            break;
-        case '-':
-            result = num1 - num2;
-            cout << num1 << " - " << num2 << " = " << result << endL;
-            break;
-        case '*':
-            result = num1 * num2;
-            cout << num1 << " * " << num2 << " = " << result << endL;
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                cout << num1 << " / " << num2 << " = " << result << endL;
-            }
-            else
-                cout << "INVALID! Division by zero." << endL;
-            break;
-        default:
-            cout << "Invalid operation! Please choose from (+, -, *, /)." << endL;
-            break;
-    }
+        cout << "Do you want to continue (y/n)? ";
+        cin >> continueChoice;
+    } while (continueChoice == 'y' || continueChoice == 'Y');
+
+    cout << "Thank you for using the calculator. Goodbye!" << endl;
 }
 
 int main() {
     solution();
     return 0;
 }
-
-
-
-
